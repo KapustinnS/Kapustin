4a5adb465113e87ca616b50fbb21591a5e746310
KapustinnS <renzan@mail.ru>
2017-10-17 20:32:12 +0600

    Лабораторная 1 Сделано задание 1,2,3,4

---
+++ Lab1/Zadanie4/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String a;
+            a = Console.ReadLine();
+            Console.Write("Программирование это ");
+            Console.Write(a);
+            Console.Write("!");
+        }
+    }
+}
---
+++ Lab1/Zadanie5/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String a;
+            a = Console.ReadLine();
+            Console.Write("начало");
+            Console.Write(a);
+        }
+    }
+}
