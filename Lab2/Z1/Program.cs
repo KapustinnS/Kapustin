5d173e47e8f34753ee3f21709d8fc97109812b50
KapustinnS <renzan@mail.ru>
2017-10-23 23:29:50 +0600

    Lab2 Z1

---
+++ Lab2/Z1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Z1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String str;
+            str = Console.ReadLine();
+            double x;
+            x = double.Parse(str);
+            double result;
+            result = x + 7;
+            Console.Write("{0:f4}", result);
+        }
+    }
+}
