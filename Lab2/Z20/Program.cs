b60e8e4c1ddc3bd91b998506f273b7b2a0ae3f11
KapustinnS <renzan@mail.ru>
2017-10-28 10:42:41 +0600

    1-21

---
+++ Lab2/Z20/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Z20
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string t = Console.ReadLine();
+            double m = double.Parse(t);
+            t = Console.ReadLine();
+            double v = double.Parse(t);
+            t = Console.ReadLine();
+            double h = double.Parse(t);
+            double g;
+            g = 9.8067;
+            double kineticEnergy, potentialEnergy;
+            if (m < 0)
+            { Console.Write("Значение mass должно быть положительным\n" +
+                "");
+                return;
+            }
+            if (v < 0)
+            { Console.Write("Значение velocity должно быть положительным\n" +
+                "");
+                return;
+            }
+            if (h < 0)
+            {
+                Console.Write("Значение height должно быть неотрицательным\n" +
+                  "");
+                return;
+            }
+            kineticEnergy = (m*v*v)/2;
+            potentialEnergy = m * g * h;
+            Console.Write("Кинетическая энергия составляет: {0:F4} Дж\n" +
+                "Потенциальная энергия составляет: {1:F4} Дж\n" +
+                "", kineticEnergy, potentialEnergy);
+        }
+    }
+}
---
+++ Lab2/Z21/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Z21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string x = Console.ReadLine();
+            double r1 = double.Parse(x);
+            
+            x = Console.ReadLine();
+            double r2 = double.Parse(x);
+            double result;
+            if (r1 == 0)
+            {
+                Console.Write("Значение r1 должно быть не равно нулю\n" +
+                  "");
+                return;
+            }
+            if (r2 == 0)
+            {
+                Console.Write("Значение r2 должно быть не равно нулю\n" +
+                  "");
+                return;
+            }   
+            result = (1 / r1) + (1 / r2);
+            Console.Write("{0:F4}\n" +
+                "", result);
+        }
+    }
+}
