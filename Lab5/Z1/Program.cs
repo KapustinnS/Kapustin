efb510dd17ff37cc36d92f236fbfadcfbed6a227
KapustinnS <renzan@mail.ru>
2017-10-31 21:55:16 +0600

    1-2

---
+++ Lab5/Z1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Z1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = double.Parse(Console.ReadLine());
+            double b = double.Parse(Console.ReadLine());
+            if (a > b)
+            { Console.Write("Первое\n");
+            }
+            else
+            { Console.Write("Второе\n");
+            }
+        }
+    }
+}
---
+++ Lab5/Z2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Z2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double p = double.Parse(Console.ReadLine());
+            double q = double.Parse(Console.ReadLine());
+            if (q < p)
+            {
+                Console.Write("Максимальное " + p + ", Минимальное " + q);
+            }
+            else
+            {
+                Console.Write("Максимальное " + q + ", Минимальное " + p);
+            }
+        }
+    }
+}
