1a8b7c0d5414f68c04f4eb7bc10220212a6cb48a
KapustinnS <renzan@mail.ru>
2017-10-31 21:54:40 +0600

    1-37

--- Lab2/Z33/Program.cs
+++ Lab2/Z33/Program.cs
@@ -19,7 +19,7 @@ namespace Z33
             {
                 Console.Write("Высота над уровнем Земли должна быть неотрицательна\n");
                     return; }
-            z = Math.Sqrt(Math.Pow(r, 2) + Math.Pow(x, 2) - Math.Pow(r, 2));
+            z = Math.Sqrt(Math.Pow(r + x, 2) - Math.Pow(r, 2));
             Console.Write("{0:F4}\n", z);
         }
     }
