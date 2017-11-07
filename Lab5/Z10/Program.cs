b83d6fa573d4a58cf66ef9d55e69bb3f78cfa591
KapustinnS <renzan@mail.ru>
2017-11-07 15:51:22 +0600

    1-10

---
+++ Lab5/Z10/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Z10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x = double.Parse(Console.ReadLine());
+            double y = double.Parse(Console.ReadLine());
+            double z = double.Parse(Console.ReadLine());
+            double a, b, c;
+            if (x > 0)
+            {
+                a = Math.Pow(x, 2);
+            }
+            else
+            { a = x;
+            }
+            if (y > 0)
+            {
+                b = Math.Pow(y, 2);
+            }
+            else
+            {
+                b = y;
+            }
+            if (z > 0)
+            {
+                c = Math.Pow(z, 2);
+            }
+            else
+            {
+                c = z;
+            }
+            Console.Write(a+";"+b+";"+c);
+        }
+    }
+}
---
+++ Lab5/Z9/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Z9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double Sc = double.Parse(Console.ReadLine());
+            double Ss = double.Parse(Console.ReadLine());
+            double Sc1, Ss1, z; 
+            if (Sc < 0)
+            {
+                Console.Write("Площадь круга должна быть положительной\n");
+                return;
+            }
+            if (Ss < 0)
+            {
+                Console.Write("Площадь квадрата должна быть положительной\n");
+                return;
+            }
+            Sc1 = Math.Sqrt(Sc / Math.PI);
+            Ss1 = Math.Sqrt(Ss);
+            z = Sc1 * 2;
+            if (z < Ss1)
+            {
+                Console.Write("Круг уместится в квадрате\n");
+            }
+            else
+            {
+                Console.Write("Круг не поместится в квадрате\n");
+            }
+        }
+    }
+}
