b96d35fad9d95ed5da35d477acf8da58b4319e55
Kapusinns <renzan@mail.ru>
2017-11-12 16:32:44 +0600

    1-18

---
+++ Lab5/Z17/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Z17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int M = int.Parse(Console.ReadLine());
+            int P = int.Parse(Console.ReadLine());
+            if (P == 0)
+            {
+                Console.Write("Значение P дожно быть не равно нулю\n");
+                return;
+            }
+            double z, a;
+            a = M % P;
+            if (a == 0)
+            {
+                z = M / P;
+                Console.Write(z);
+            }
+            else
+            {
+                Console.Write("M не делится нацело на P\n");
+            }
+        }
+    }
+}
---
+++ Lab5/Z18/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Z18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = double.Parse(Console.ReadLine());
+            if (a < 1000 || a > 9999)
+            { Console.Write("Значение A дожно быть в интервале [1000, 9999]\n");
+                return;
+            }
+            if ((a % 1000 - (a % 1000) % 100) / 100 == a % 10)
+            {
+                Console.Write("равно\n");
+            }
+            else
+            {
+                Console.Write("не равно\n");
+            }
+        }
+    }
+}
