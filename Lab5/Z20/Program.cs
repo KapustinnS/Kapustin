2c6e5843ed2050bba5c5505fc6b7f932e0165d26
Kapusinns <renzan@mail.ru>
2017-11-13 20:19:28 +0600

    1-20

---
+++ Lab5/Z20/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Z20
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x = double.Parse(Console.ReadLine());
+            double y = double.Parse(Console.ReadLine());
+            if (y >= -2 && y <= 1.5)
+            {
+                Console.Write("Точка принадлежит множеству\n");
+            }
+            else
+            {
+                Console.Write("Точка не принадлежит множеству\n");
+            }
+        }
+    }
+}
