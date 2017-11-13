d5f3cd212c6b5fafeab9fe71409660d3904f27ec
Kapusinns <renzan@mail.ru>
2017-11-13 20:01:44 +0600

    1-19

---
+++ Lab5/Z19/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Z19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x = double.Parse(Console.ReadLine());
+            double y = double.Parse(Console.ReadLine());
+            if (x <= -2 && y >= 1)
+            {
+                Console.Write("Точка принадлежит множеству\n");
+
+            }
+            else
+            {
+                Console.Write("Точка не принадлежит множеству\n");
+            }
+        }
+    }
+}
