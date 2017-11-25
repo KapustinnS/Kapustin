82401dafe4c0e6b1eb3f0273b75b3325735a4e3f
KapustinnS <renzan@mail.ru>
2017-11-25 20:22:16 +0600

    1-15

---
+++ Lab6/Z15/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Z15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = 20;
+            double v;
+            double s = 0;
+            Console.Write("Ход решения: 0+");
+            while (a <= 40)
+            {
+                v = Math.Pow(a, 3);
+                Console.Write(v + "=");
+                s += v;
+                Console.Write(s +"+");
+                a++;
+            }
+            Console.WriteLine("0 = ");
+            Console.WriteLine("Ответ: " + s);
+        }
+    }
+}
