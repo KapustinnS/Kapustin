e4422c136e464c1176d365040e78af0e593fd15c
KapustinnS <renzan@mail.ru>
2017-12-05 13:49:59 +0600

    1-20

---
+++ Lab6/Z19/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Z19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 0;
+            int right = 0;
+            int left = 0;
+            while (a <= 83 || a >= 199)
+            {
+                a = int.Parse(Console.ReadLine());
+
+                if (a <= 83 || a >= 199)
+                {
+                    if (a >= 199)
+                    {
+                        right++;
+                    }
+                    if (a <= 83)
+                    {
+                        left++;
+                    }
+                }
+            }
+            Console.WriteLine("Количество слева: " + left + ",количества справа: " + right);
+        }
+    }
+}
---
+++ Lab6/Z20/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Z20
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = double.Parse(Console.ReadLine());
+            double b = double.Parse(Console.ReadLine());
+            double sum = 0;
+            if (a >= b)
+            {
+                while (b <= a)
+                {
+                    sum += Math.Pow(a,2);
+                    a--;
+
+                }
+                Console.WriteLine(sum);
+            }
+            else
+            {
+                while (a <= b)
+                {
+                    sum += Math.Pow(a,2);
+                    a++;
+                }
+                Console.WriteLine(sum);
+            }
+        }
+    }
+}
