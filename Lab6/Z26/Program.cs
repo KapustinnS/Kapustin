8c5a3c8a278c3cb63b7e4a2ee05e0ef63ef1fd56
KapustinnS <renzan@mail.ru>
2017-12-08 15:00:06 +0600

    1-26

---
+++ Lab6/Z26/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Z26
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 0;
+            int a = 100;
+            int b = 1000;
+            Console.Write("Ход решения: ");
+            while ( a <= b)
+            {
+                if (a % 13 != 0)
+                {
+                    Console.Write(a + " ");
+                }
+                else
+                {
+                    Console.Write(a + "+ ");
+                    x++;
+                }
+                a++;
+            }
+            Console.WriteLine("Ответ: " + x);
+        }
+    }
+}
