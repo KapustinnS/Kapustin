af685b26c24625f183af24d396352d1d4d7967d2
KapustinNs <renzan@mail.ru>
2017-12-12 20:27:45 +0600

    1-34

--- Lab6/Z31/Program.cs
+++ Lab6/Z31/Program.cs
@@ -12,81 +12,23 @@ namespace Z31
         {
             int w = int.Parse(Console.ReadLine());
             int i = 0;
+            int j = 0;
             if (w < 0 || w > 20)
             {
                 Console.WriteLine("Значение W должно быть в интервале[0, 20]");
                 return;
             }
-            else
+            while (j < 10)
+            {
                 while (i < w)
                 {
-                    Console.Write("0");
+                    Console.Write(j);
                     i++;
                 }
-            Console.WriteLine();
-            i = 0;
-            while (i < w)
-            {
-                Console.Write("1");
-                i++;
+                Console.WriteLine();
+                i = 0;
+                j++;
             }
-            Console.WriteLine();
-            i = 0;
-            while (i < w)
-            {
-                Console.Write("2");
-                i++;
-            }
-            Console.WriteLine();
-            i = 0;
-            while (i < w)
-            {
-                Console.Write("3");
-                i++;
-            }
-            Console.WriteLine();
-            i = 0;
-            while (i < w)
-            {
-                Console.Write("4");
-                i++;
-            }
-            Console.WriteLine();
-            i = 0;
-            while (i < w)
-            {
-                Console.Write("5");
-                i++;
-            }
-            Console.WriteLine();
-            i = 0;
-            while (i < w)
-            {
-                Console.Write("6");
-                i++;
-            }
-            Console.WriteLine();
-            i = 0;
-            while (i < w)
-            {
-                Console.Write("7");
-                i++;
-            }
-            Console.WriteLine();
-            i = 0;
-            while (i < w)
-            {
-                Console.Write("8");
-                i++;
-            }
-            Console.WriteLine();
-            i = 0;
-            while (i < w)
-            {
-                Console.Write("9");
-                i++;
-            }
-            Console.WriteLine();
         }
     }
 }
---
+++ Lab6/Z34/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Z34
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int w = int.Parse(Console.ReadLine());
+            int h = int.Parse(Console.ReadLine());
+            int i = 0;
+            int j = 0;
+            if (w < 0)
+            {
+                Console.WriteLine("Значение W должно быть неотрицательно");
+                return;
+            }
+            if (h < 0)
+            {
+                Console.WriteLine("Значение H должно быть неотрицательно");
+                return;
+            }
+            if (w == 0)
+            {
+                return;
+            }
+            if (h == 0)
+            {
+                return;
+            }
+            Console.Write(" ");
+
+            while (i < w)
+            {
+                Console.Write(i);
+                i++;
+            }
+            Console.WriteLine();
+            while (j < h)
+            {
+                i = 0;
+                Console.Write(j);
+                while (i < w)
+                {
+
+                    Console.Write(".");
+                    i++;
+                }
+
+                Console.Write("|");
+                Console.WriteLine();
+                j++;
+
+            }
+            i = 0;
+            Console.Write(" ");
+            while (i < w)
+            {
+                Console.Write("_");
+                i++;
+            }
+        }
+    }
+}
+
