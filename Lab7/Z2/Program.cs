d2d6433caf68f7d92ed23d1332d30fcf191f291d
KapustinnS <renzan@mail.ru>
2018-01-23 20:20:42 +0600

    1-3

---
+++ Lab7/Z2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Z2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string data = "abcdefwxyz";
+            string x = Console.ReadLine();
+            int i = data.IndexOf(x); //ячейка массива
+            if ( i < 0)
+            {
+                Console.WriteLine("Значение отсутствует в строке");
+            }
+            else
+            {
+                int result = data.Length - i - 1;
+                Console.WriteLine(result);
+            }
+        }
+    }
+}
---
+++ Lab7/Z3/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Z3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string data = "abcdefwxyz";
+            string CA = Console.ReadLine();
+            string CB = Console.ReadLine();
+            int i = data.IndexOf(CA);
+            int n = data.IndexOf(CB);
+            if (i < 0)
+            {
+                Console.WriteLine("Значение CA отсутствует в строке");
+                return;
+            }
+            if (n < 0)
+            {
+                Console.WriteLine("Значение CB отсутствует в строке");
+                return;
+            }
+            int result = i - n -1;
+            Console.WriteLine(result);
+        }
+    }
+}
