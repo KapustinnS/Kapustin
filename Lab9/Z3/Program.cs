20aa0bff5a3694263e229d49d4b0b2786a97686f
KapustinnS <renzan@mail.ru>
2018-01-23 20:19:33 +0600

    1-3

--- Lab9/Z3/Program.cs
+++ Lab9/Z3/Program.cs
@@ -11,16 +11,34 @@ namespace Z3
     {
         static void Main(string[] args)
         {
+            string b = Console.ReadLine();
             int a = int.Parse(Console.ReadLine());
-            string task = "../../task4642/test" + a + ".txt";
-            if (!File.Exists(task))
+            int a1 = 0;
+            string filename = "../../task4642/test" + a + ".txt";
+            if (!File.Exists(filename))
             {
                 Console.WriteLine("Файл не существует");
                 return;
             }
+            int c = 0;
             StreamReader reader;
-            reader = new StreamReader(task);
+            reader = new StreamReader(filename);
             string line = reader.ReadLine();
+            //if (c < a1)
+            //{
+            //    while (!reader.EndOfStream)
+            //    {
+            //        Console.Write(b);
+            //        c++;
+            //    }
+            //}
+            while (!reader.EndOfStream)
+            {
+                reader.ReadLine();
+                c++;
+            }
+            Console.Write(line);
+            reader.Close();
         }
     }
 }
